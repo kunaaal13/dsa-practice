9106ee826d85a216b1b0206b9a744ce4d85b671b
Kunaaal13 <mrkunalyadav7@gmail.com>
2022-10-08 21:22:27 +0530

    day 1

--- src/com/kunaaal/Arrays/IsSubsequence.java
+++ src/com/kunaaal/Arrays/IsSubsequence.java
@@ -1,7 +1,5 @@
 package com.kunaaal.Arrays;
 
-import java.util.HashSet;
-
 public class IsSubsequence {
     public static void main(String[] args) {
         String s = "aec";
@@ -11,27 +9,15 @@ public class IsSubsequence {
     }
 
     public static boolean isSubsequence(String s, String t) {
-        HashSet<Character> set = new HashSet<>();
+        if (s.length() == 0)
+            return true;
 
-        for (int i = 0; i < s.length(); i++) {
-            set.add(s.charAt(i));
+        for(int i=0 , j=0;i<t.length();i++){
+            if(t.charAt(i)==s.charAt(j))
+                j++;
+            if(j==s.length())
+                return true;
         }
-
-        int i = 0;
-        int j = 0;
-
-        while (i < s.length() && j < t.length()){
-            if (s.charAt(i) == t.charAt(j)){
-                i++;
-            } else {
-                // does not match
-                if (set.contains(t.charAt(j)))
-                    return false;
-            }
-            j++;
-        }
-
-
-        return true;
+        return false;
     }
 }
---
+++ src/com/kunaaal/Arrays/LengthLastWord.java
@@ -0,0 +1,33 @@
+package com.kunaaal.Arrays;
+
+public class LengthLastWord {
+    public static void main(String[] args) {
+        String s = "luffy is still joyboy";
+
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        boolean isWord = false;
+
+        for (int i = s.length()-1; i >= 0; i--) {
+            if (!isWord){
+                if (s.charAt(i) != ' '){
+                    length++;
+                    isWord = true;
+                }
+            } else {
+                if (s.charAt(i) == ' '){
+                    return length;
+                } else {
+                    length++;
+                }
+            }
+        }
+
+
+
+        return length;
+    }
+}
