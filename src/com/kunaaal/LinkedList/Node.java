af147723ad3d982ed8a59359d19ffa21d966abec
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-01-02 17:28:57 +0530

    day 20

---
+++ src/com/kunaaal/LinkedList/Node.java
@@ -0,0 +1,17 @@
+package com.kunaaal.LinkedList;
+
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
